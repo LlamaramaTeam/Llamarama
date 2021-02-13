6155e89421321b0e20f3686ef66aa62dc7191e0b
PeterGamesGR <46004440+PeterGamesGR@users.noreply.github.com>
2021-02-13 15:37:44 +0200

    Peter does not know how to exclude stuff from server jars...

--- src/main/java/com/github/llamamod/team/LlamaModClient.java
+++ src/main/java/com/github/llamamod/team/LlamaModClient.java
@@ -1,7 +1,11 @@
 package com.github.llamamod.team;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
+
+@Environment(EnvType.CLIENT)
 public class LlamaModClient implements ClientModInitializer {
 
     @Override
