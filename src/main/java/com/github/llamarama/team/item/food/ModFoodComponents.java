cdbf5f593e2e4f1417d4e643a27e53d598bbe7c6
KaloyanKKS <k.yunchov9@gmail.com>
2021-02-24 18:07:32 +0000

    milk

--- src/main/java/com/github/llamarama/team/item/food/ModFoodComponents.java
+++ src/main/java/com/github/llamarama/team/item/food/ModFoodComponents.java
@@ -8,6 +8,7 @@ public final class ModFoodComponents {
 
     public static final FoodComponent RAW_LLAMA_MEAT = new FoodComponent.Builder().hunger(4).saturationModifier(2).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 20 * 20), 0.4f).meat().build();
     public static final FoodComponent ROASTED_LLAMA_MEAT = new FoodComponent.Builder().hunger(8).saturationModifier(10).meat().build();
+    public static final FoodComponent LLAMA_MILK = new FoodComponent.Builder().hunger(2).saturationModifier(4).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 20 * 20), 0.4f).build();
 
     private ModFoodComponents() {
 
