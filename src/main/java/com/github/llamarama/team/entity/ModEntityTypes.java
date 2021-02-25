eaef1990b73a3ca2dfc1f7681c0cdacb7afe3d3d
PeterGamesGR <46004440+PeterGamesGR@users.noreply.github.com>
2021-02-25 19:59:34 +0200

    Utilities.
    Signed-off-by: PeterGamesGR <46004440+PeterGamesGR@users.noreply.github.com>

--- src/main/java/com/github/llamarama/team/entity/ModEntityTypes.java
+++ src/main/java/com/github/llamarama/team/entity/ModEntityTypes.java
@@ -1,27 +1,24 @@
 package com.github.llamarama.team.entity;
 
-import com.github.llamarama.team.Llamarama;
 import com.github.llamarama.team.entity.woolyllama.WoollyLlamaEntity;
+import com.github.llamarama.team.util.IDBuilder;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 import java.util.function.Supplier;
 
+@SuppressWarnings("SameParameterValue")
 public final class ModEntityTypes {
 
-    public static final EntityType<WoollyLlamaEntity> WOOLLY_LLAMA = Registry.register(Registry.ENTITY_TYPE, new Identifier(Llamarama.MOD_ID, "woolly_llama"), FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, WoollyLlamaEntity::new).dimensions(new EntityDimensions(0.9f, 1.87f, false)).trackRangeBlocks(10).build());
+    public static final EntityType<WoollyLlamaEntity> WOOLLY_LLAMA = register(WoollyLlamaEntity::new, SpawnGroup.CREATURE, 0.9f, 1.87f, false, 10, "woolly_llama");
     private static ModEntityTypes instance;
 
 
     private ModEntityTypes() {
-        register(WOOLLY_LLAMA, WoollyLlamaEntity::createLlamaAttributes);
+        registerAttributes(WOOLLY_LLAMA, WoollyLlamaEntity::createLlamaAttributes);
     }
 
     public static void init() {
@@ -30,7 +27,11 @@ public final class ModEntityTypes {
         }
     }
 
-    private void register(EntityType<? extends LivingEntity> type, Supplier<DefaultAttributeContainer.Builder> builderSupplier) {
+    private static <T extends Entity> EntityType<T> register(EntityType.EntityFactory<T> factory, SpawnGroup group, float width, float height, boolean fixed, int range, String id) {
+        return Registry.register(Registry.ENTITY_TYPE, IDBuilder.of(id), FabricEntityTypeBuilder.create(group).entityFactory(factory).dimensions(new EntityDimensions(width, height, fixed)).trackRangeBlocks(range).build());
+    }
+
+    private void registerAttributes(EntityType<? extends LivingEntity> type, Supplier<DefaultAttributeContainer.Builder> builderSupplier) {
         FabricDefaultAttributeRegistry.register(type, builderSupplier.get());
     }
 
