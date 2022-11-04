3ff91783ffa48c87ff5bfd34e7a2b3e57a2fdcb0
KaloyanKKS <k.yunchov9@gmail.com>
2022-11-04 19:37:06 +0000

    Much needed texture update; Rug model is finally fixed to not use single pixel planes - should be better for performance but is probably negligible.

--- src/main/java/io/github/llamarama/team/client/LlamaramaClient.java
+++ src/main/java/io/github/llamarama/team/client/LlamaramaClient.java
@@ -3,11 +3,14 @@ package io.github.llamarama.team.client;
 import io.github.llamarama.team.client.item.LlamaWoolBedBlockEntityItemRenderer;
 import io.github.llamarama.team.client.register.ModBlockEntityRenderers;
 import io.github.llamarama.team.client.register.ModEntityRenderers;
+import io.github.llamarama.team.common.register.ModBlocks;
 import io.github.llamarama.team.common.register.ModItems;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
+import net.minecraft.client.render.RenderLayer;
 
 @Environment(EnvType.CLIENT)
 public class LlamaramaClient implements ClientModInitializer {
@@ -23,6 +26,9 @@ public class LlamaramaClient implements ClientModInitializer {
 
         // Register the llama wool bed item custom rendering.
         BuiltinItemRendererRegistry.INSTANCE.register(ModItems.LLAMA_WOOL_BED, new LlamaWoolBedBlockEntityItemRenderer());
+
+        // Set rug blocks to render as cutout
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RUG, RenderLayer.getCutout());
     }
 
 }
--- src/main/java/io/github/llamarama/team/common/item/LlamaMilkItem.java
+++ src/main/java/io/github/llamarama/team/common/item/LlamaMilkItem.java
@@ -1,7 +1,15 @@
 package io.github.llamarama.team.common.item;
 
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.item.MilkBucketItem;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.world.World;
 
 public class LlamaMilkItem extends MilkBucketItem {
 
@@ -9,5 +17,19 @@ public class LlamaMilkItem extends MilkBucketItem {
         super(settings);
     }
 
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        if (user instanceof ServerPlayerEntity serverPlayerEntity) {
+            Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
+            serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+        }
+
+        if (user instanceof PlayerEntity && !((PlayerEntity)user).getAbilities().creativeMode) {
+            stack.decrement(1);
+        }
+
+        if (!world.isClient) {
+            user.clearStatusEffects();
+        } return stack.isEmpty() ? new ItemStack(Items.BOWL) : stack;
+    }
 }
 
